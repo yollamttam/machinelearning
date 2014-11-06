01aba930127d0c86a2803a6fdfe90201a3ea5f56
Matt Malloy <yollam.ttam@gmail.com>
2014-11-06 11:29:13 -0500

    testing...

--- mlclass-ex4-007/mlclass-ex4/nnCostFunction.m
+++ mlclass-ex4-007/mlclass-ex4/nnCostFunction.m
@@ -24,7 +24,7 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
+%this is a wasted line         
 % You need to return the following variables correctly 
 J = 0;
 Theta1_grad = zeros(size(Theta1));
